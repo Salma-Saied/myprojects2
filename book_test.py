dbd7a5405fff460876071d26c36fba1683accd61
salma <salmasaed57@gmail.com>
2025-02-03 08:07:06 -0800

    adding fuction from the book

---
+++ book_test.py
@@ -0,0 +1,24 @@
+# كتاب البايثون
+#  we can put a comma between string and integer as we do with variable
+print("salma",100-25*3/4)
+
+# True or False
+print(10>5)
+print("is it greater or equal?", 5>=-2)
+print(f"is it greate or equal? {5>=-2}")
+
+#  DECIMAL Library to solve this problem
+print(0.1+0.2)
+
+# the solution by using DECIMAL Library
+from decimal import Decimal
+x=Decimal("0.1")+Decimal("0.2")
+print(x)
+
+# %S يستخدم للتنسيق ويحول اي نوع بيانات ل string لكن ال f string احدث
+
+
+
+
+
+
